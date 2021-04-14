c58abff488f4fa5b6d7577a69f9e2fe3b0268ba0
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-04-14 17:19:20 +0300

    Add predict_proba function to measure cross-entropy.

--- pattern_clf.py
+++ pattern_clf.py
@@ -1,12 +1,14 @@
 from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.extmath import softmax
 import random
 import numpy as np
 import pandas as pd
 
 
 class LazyPatternClassifier(BaseEstimator, ClassifierMixin):
-    def __init__(self, tolerance=0.0):
+    def __init__(self, tolerance=0.0, use_softmax=True):
         self.tolerance = tolerance
+        self.use_softmax = use_softmax
 
 
     def fit(self, X: pd.DataFrame, y: pd.Series):
@@ -27,6 +29,23 @@ class LazyPatternClassifier(BaseEstimator, ClassifierMixin):
         for i in range(X.shape[0]):
             y_pred[i] = self._predict_one(Xnum[i], Xcat[i])
         return y_pred
+
+
+    def predict_proba(self, X: pd.DataFrame):
+        result = np.empty((X.shape[0], 2))
+        Xnum = X.select_dtypes(include=float).values
+        Xcat = X.select_dtypes(exclude=float).values
+        for i, (num, cat) in enumerate(zip(Xnum, Xcat)):
+            result[i] = (self._score(num, cat, False), self._score(num, cat, True))
+
+        if self.use_softmax:
+            softmax(result, copy=False)
+        else:
+            s = result.sum(axis=1, keepdims=True)
+            mask = (s.squeeze() != 0)
+            result[mask] /= s[mask]
+            result[~mask] = 0.5
+        return result
 
 
     def _predict_one(self, num: np.ndarray, cat: np.ndarray) -> bool:
