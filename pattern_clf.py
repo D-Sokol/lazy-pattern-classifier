f99744d7c038d3a75a2433a75a79fcd31bf0e178
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-05-24 23:47:54 +0300

    Removed deprecated method.

--- pattern_clf.py
+++ pattern_clf.py
@@ -28,16 +28,6 @@ class LazyPatternClassifier(BaseEstimator, ClassifierMixin):
             y_pred[i] = self._predict_one(Xnum[i])
         return y_pred
 
-    def predict_proba(self, X: pd.DataFrame):
-        result = np.empty((X.shape[0], 2))
-        Xnum = X.select_dtypes(include=float).values
-        assert X.select_dtypes(exclude=float).values.size == 0
-        for i, num in enumerate(Xnum):
-            result[i] = (self._score(num, False), self._score(num, True))
-
-        softmax(result, copy=False)
-        return result
-
     def _predict_one(self, num: np.ndarray) -> bool:
         clfs = self.Xnum_p if self.use_positive else self.Xnum_n
         objs = self.Xnum_n if self.use_positive else self.Xnum_p
@@ -57,24 +47,3 @@ class LazyPatternClassifier(BaseEstimator, ClassifierMixin):
     def _satisfy(pattern_mins, pattern_maxs, other_num):
         mask = np.logical_and((other_num >= pattern_mins), (other_num <= pattern_maxs)).all(axis=1)
         return mask
-
-    # deprecated
-    def _score(self, num, to_positive=True):
-        this_num = self.Xnum_p if to_positive else self.Xnum_n
-        other_num = self.Xnum_n if to_positive else self.Xnum_p
-        this_w = self.weights_p if to_positive else self.weights_n
-        other_w = self.weights_n if to_positive else self.weights_p
-
-        votes = 0
-        for bnum, weight in zip(this_num, this_w):
-            pattern = self._get_pattern(num, bnum)
-            mask = self._satisfy(*pattern, other_num)
-
-            if self.weight_classifiers:
-                if mask.mean() <= self.tolerance:
-                    votes += weight
-            else:
-                if mask @ other_w <= self.tolerance:
-                    votes += 1
-
-        return votes / len(this_num)
